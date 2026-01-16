eb9e1cb7ab201358796d58300f570b99e81226da
Panyut Sriwirote <mildpanyut@gmail.com>
2026-01-17 00:14:22 +0700

    feat: add board resolution

--- internal/rule/rule.go
+++ internal/rule/rule.go
@@ -1,11 +1,51 @@
 package rule
 
-import "github.com/panyutsriwirote/GoGo/internal/board"
+import (
+	"github.com/panyutsriwirote/GoGo/internal/board"
+	"github.com/panyutsriwirote/GoGo/internal/coord"
+)
 
 type ResolutionError struct {
 	Reason string
 }
 
 func ResolveBoard(board_state *board.BoardState) *ResolutionError {
+	next_player := board_state.NextPlayer
+	prev_player := board.SwitchPlayer(next_player)
+
+	// Remove captured groups.
+	for x := range board_state.Stones {
+		for y := range board_state.Stones[x] {
+			pos := coord.New(x, y)
+			if board_state.GetOwner(pos) == next_player {
+				group := board_state.GetGroup(pos)
+				group_liberty := board_state.GetGroupLiberty(group)
+				if group_liberty == 0 {
+					board_state.RemoveGroup(group)
+					board_state.AddPrisoner(prev_player, len(group))
+				}
+			}
+		}
+	}
+
+	// Check suicide.
+	for x := range board_state.Stones {
+		for y := range board_state.Stones[x] {
+			pos := coord.New(x, y)
+			if board_state.GetOwner(pos) == prev_player {
+				group := board_state.GetGroup(pos)
+				group_liberty := board_state.GetGroupLiberty(group)
+				if group_liberty == 0 {
+					return &ResolutionError{"Suicide"}
+				}
+			}
+		}
+	}
+
+	// Check Ko.
+	if board_state.Stones == board_state.Prev.Stones {
+		return &ResolutionError{"KO"}
+	}
+
 	return nil
 }
